88c1e9bb761356711b8de480ae50d31525613b54
Juandisk <152462640+Juandisk@users.noreply.github.com>
2025-09-01 17:47:45 -0500

    feet: sparkles:

---
+++ Proyecto.py
@@ -0,0 +1,14 @@
+import json
+import os
+import sys
+
+favoritas = []
+historial = []
+
+def main():
+    print("🎬 Iniciando Netflix Console...")
+    
+    print("✅ Programa iniciado correctamente")
+
+if __name__ == "__main__":
+    main()

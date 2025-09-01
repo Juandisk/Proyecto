2f282a3b7d17b30c7b82a1a79d1b6686cb89f62b
Juandisk <152462640+Juandisk@users.noreply.github.com>
2025-09-01 16:56:52 -0500

    feet: sparkle:

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ with open('peliculas.json', 'r', encoding='utf-8') as archivo:
     return peliculas    
 
 except json.JSONDecodeError as e:
-    print("Error: El archivo JSON tiene un formato incorrecto")
-    print(f"Detalle del error: {e}")
+        print("Error: El archivo JSON tiene un formato incorrecto")
+        print(f"Detalle del error: {e}")
     return None
  
 except Exception as e: 
@@ -60,4 +60,28 @@ def mostrar_estadisticas_carga:
     mejor_rating = 0 
     
     for genero in peliculas.values(): 
-                
+        for peliculas in genero:
+            if pelicula['rating'] > mejor_rating: 
+                mejor_rating = peliculas ['rating']
+                mejor_peliculas = peliculas
+    if mejor_pelicula:
+        print(f"Mejor calificada: {mejor_pelicula['titulo']}({mejor_rating}/10)")
+
+def main():
+    print("Netflix conxole - cargando datos...")
+    print("=" * 50)
+
+    prin("cargando peliculas desde peliculas.json...")
+    peliculas = cargar_peliculas
+
+    if peliculas:
+        print("Peliculas cargadas exitosamente")
+        mostrar_estadisticas_carga(peliculas) 
+    else: 
+        print("Error: No se pudieron cargar las peliculas")
+        print("Revisa que el archivo peliculas.json estè en la carpeta correcta")
+        return
+    print("\n Sistema listo para usar")
+
+if__name__ == "__main__":
+    main()
